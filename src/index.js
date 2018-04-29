b0d63efeabad346639d23ebee0bbebf5591d6b99
Vikas Rajoria <vrajoria@walmartlabs.com>
2018-04-30 01:57:36 +0530

    Youtube player

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDYh0lxX-h4uxykAsJkDypcBzzi8r4ohhc';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo : null
+    }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'Bazz'}, videos => {
+      console.log(videos);
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    })
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
