e31db957f9b8477407ad132a5a9149cbe0ce49dd
Vikas Rajoria <vrajoria@walmartlabs.com>
2018-05-02 12:23:39 +0530

    React section done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash'; 
 
 const API_KEY = 'AIzaSyDYh0lxX-h4uxykAsJkDypcBzzi8r4ohhc';
 
@@ -14,8 +15,11 @@ class App extends Component {
       videos: [],
       selectedVideo : null
     }
+    this.videoSearch("");
+  }
 
-    YTSearch({key: API_KEY, term: 'Bazz'}, videos => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
       console.log(videos);
       this.setState({ 
         videos: videos,
@@ -23,10 +27,12 @@ class App extends Component {
        });
     })
   }
+
   render () {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
